918a8cd445a38481740e11a60391419575272454
Biplob Dash <110329225+BiplobDash@users.noreply.github.com>
2023-11-18 19:26:41 +0600

    Firebase Add

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/responsive/mobile_screen_layout.dart';
 import 'package:instagram_clone/responsive/responsive_layout_screen.dart';
 import 'package:instagram_clone/responsive/web_screen_layout.dart';
 import 'package:instagram_clone/utill/colors.dart';
 
-void main() => runApp(const MyApp());
-
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp(),);
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
